72ac7b2de3f4ed4405d2726925d526a17d4b57dc
vierbein <vierbein@penguin>
2020-10-10 21:40:45 -0400

    Fix for the change in GA DPH website

--- fetch.py
+++ fetch.py
@@ -1,75 +1,80 @@
 #!/usr/bin/env python
 """
 Fetch JSON from the Georgia DPH COVID-19 daily status report[^1].
-Note the website actually displays contents by the SAS institute[^2].
+Note the website actually displays contents from the SAS institute (sas.com)[^2].
 
 [^1] https://dph.georgia.gov/covid-19-daily-status-report
 [^2] https://ga-covid19.ondemand.sas.com/
 
 
-As of 2020-09-15, 23 JSON strings are embedded in the JavaScript code.
+As of 2020-10-10, 18 JSON strings are embedded in the JavaScript code.
 
-00 ... county geometry
-01 ... datetime of the last update
-02 ... sequence of positives and death counts in GA and counties
-03 ... sequence of positives and death counts in GA and counties (same keys, different numbers)  ... Displayed as "COVID-19 By County" and "COVID-19 Over Time"
-04 ... sequence of PCR test counts in GA and counties
-05 ... county-wise summary (subset of #07)
-06 ... individual death (age, race, sex, county, chronic condition)
-07 ... county-wise summary
-08 ... IQR (interquartile range) summary
-09 ... case and death by race and ethnicity
-10 ... case and death totals by race, ethnicity, sex, age group, and county
-11 ... lab testing over time ... displayed as "Lab Testing"
-12 ... lab testing summary
-13 ... current total counts
-14 ... increments in total counts
-15 ... positives and hospitalizations by test date, age group, race, ethnicity, sex, county, positive patient data (35070 out of 296833 as of 2020-09-15)
-16 ... sequence of positives and death by age group
-17 ... sequence of positives, death, and hospitalization by ethnicity, race, and sex
-18 ... current totals by ethnicity, race, and sex
-19 ... current totals by comorbidity and sex
-20 ... current totals by ethnicity, race, sex, and comorbidity
-21 ... current case count by binary comorbidity (Yes/No/Unknown)
-22 ... current test counts (pcr and antibody)
-23 ... lab testing summary (added since 2020-09-22)
+00 ... timestamp of the last update
+01 ... history of cases and deaths in GA counties
+02 ... total counts in GA and counties
+03 ... individual death (age, race, sex, county, chronic condition)
+04 ... case and death by race and ethnicity
+05 ... case and death totals by race, ethnicity, sex, age group, and county
+06 ... lab testing over time ... displayed as "Lab Testing"
+07 ... lab testing summary
+08 ... current total counts (total_tests, confirmed_covid, icu, hospitalization, death)
+09 ... current changes (diff in positives, hospitalizations, deaths, tests, total_tests, confirmed_covid, icu, hospitalization, death)
+10 ... history of cases, deaths, hospitalization by age group, race, ethnicity, sex
+11 ... history of cases and deaths by age group
+12 ... history of cases and deaths by ethnicity, race, and sex
+13 ... case and death counts by race, ethnicity, sex, and county
+14 ... count by comorbidity and sex
+15 ... count by ethnicity, race, sex, and comorbidity
+16 ... count by ternary comorbidity (Yes/No/Unknown)
+17 ... test counts (tot, pcrtest, abtest, pcrpos, abpos)
+
 """
 
 import urllib.request
+import urllib.parse
 import re
 import pathlib
+from html.parser import HTMLParser
+from html.entities import name2codepoint
 
-URL = "https://ga-covid19.ondemand.sas.com/static/js/main.js"
+BASE_URL = "https://ga-covid19.ondemand.sas.com/"
 PATTERN = re.compile(r"JSON.parse\('(.*?)'\)")
 
 FILENAME = [
-    "geometry_county",
-    "last_updated",
-    "cases_county_over_time0",
-    "cases_county_over_time",
-    "tests_over_time",
-    "summary_county0",
-    "deaths",
-    "summary_county",
-    "iqr",
-    "cases_by_race_ethnicity",
-    "cases_totals",
-    "lab_testing",
+    "timestamp",
+    "county_history",
+    "county_totals",
+    "individual_death",
+    "race_ethnicity",
+    "lab_testing_history",
     "lab_testing_totals",
+    "lab_testing_summary",
     "current_status",
     "current_status_changes",
-    "positive_cases",
-    "demographics",
-    "comorbidities",
-    "comorbidities_by_sex",
-    "comorbidities_by_race_and_sex",
-    "cases_tally",
-    "comorbidity_total",
-    "current_status_tests",
-    "lab_testing_summary",
+    "demographics_history",
+    "age_history",
+    "ethnicity_history",
+    "ethnicity_totals",
+    "comorbidities_totals",
+    "comorbidities_ethnicy_totals",
+    "comorbidity_summary",
+    "testing_summary",
 ]
 
 SAVE_DIR = "artifacts"
+
+
+class MyHTMLParser(HTMLParser):
+    """HTML parser that stores <script> tag,
+    src attribute, as self.url
+    """
+    def handle_starttag(self, tag, attrs):
+        if tag.lower() == "script":
+            for attr in attrs:
+                if attr[0].lower() == "src":
+                    rel = attr[1].lower()
+                    if "main" in rel:
+                        self.url = urllib.parse.urljoin(BASE_URL, rel)
 
 
 def extract_json(text: str):
@@ -80,11 +85,23 @@ def extract_json(text: str):
         yield match.group(1)
 
 
-def run(url=URL, prefix=""):
+def extract_js_url(text: str):
+    """Extract main.js url
+    """
+    parser = MyHTMLParser()
+    parser.feed(text)
+    return parser.url
+
+
+def run(url=BASE_URL, prefix=""):
     path_root = pathlib.Path(SAVE_DIR)
     path_root.mkdir(exist_ok=True)
 
     with urllib.request.urlopen(url) as response:
+        html = response.read().decode("utf-8")
+        url_js = extract_js_url(html)
+
+    with urllib.request.urlopen(url_js) as response:
         js_text = response.read().decode("utf-8")
         gen = extract_json(js_text)
         for i, s in enumerate(gen):
